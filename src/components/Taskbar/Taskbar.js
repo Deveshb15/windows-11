22498e2537fbb3f13014e3fe3e1f34e36fdc818c
Deveshb15 <deveshb583@gmail.com>
2021-06-28 20:02:43 +0530

    feat: Date image added

--- src/components/Taskbar/Taskbar.js
+++ src/components/Taskbar/Taskbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Taskbar.css'
 import taskbar from '../../assets/icons.PNG'
+import Date from "./Date/Date";
 
 const Taskbar = (props) => {
     return (
         <div className="taskbar">
             <img onClick={props.onToggle} src={taskbar} alt="taskbar"/>
+            <Date />
         </div>
     )
 }
