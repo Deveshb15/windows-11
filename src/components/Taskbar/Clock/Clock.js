345c7c01054f5d69c94cb60dfac8dfe9affecb3e
Deveshb15 <deveshb583@gmail.com>
2021-06-30 19:28:10 +0530

    battery wrong branch

--- src/components/Taskbar/Clock/Clock.js
+++ src/components/Taskbar/Clock/Clock.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import './Clock.css'
 
+import battery from '../../../assets/battery.png'
+
 const Clock = (props) => {
 
     const [timer, setTimer] = useState(new Date())
@@ -20,8 +22,13 @@ const Clock = (props) => {
 
     return (
         <div className="dateRight">
-            <p>{timer.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}</p>
-            <p>{`${timer.getMonth()+1}/${timer.getDate()}/${timer.getFullYear()}`}</p>
+            <div className="batteryDisplay">
+                <img src={battery} alt="battery" />
+            </div>
+            <div className="timeDisplay">
+                <p>{timer.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}</p>
+                <p>{`${timer.getMonth()+1}/${timer.getDate()}/${timer.getFullYear()}`}</p>
+            </div>
         </div>
     )
 }
